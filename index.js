6f1f7829331d7cc3114a07d94ea0f451b0addd7a
Abigail Paden <Abigail Paden>
2016-11-09 12:12:49 -0500

    changed myArray to Object

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let myArray = [1,2,3,4,5,6,7,8,9,0];
 
 app.get('/numbers', (req, res) => {
 	res.json(myArray);
-})
+});
 
 app.listen(3000, () => {
 	console.log("Listening on port 3000...");
-})
+});
