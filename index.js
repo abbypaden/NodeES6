7bed78cfd1bb7a52cb54cbf72bff9d36904bedc6
Abigail Paden <Abigail Paden>
2016-11-09 11:59:40 -0500

    converted to es6

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var myArray = [1,2,3,4,5,6,7,8,9,0];
+let express = require('express');
+let app = express();
+let myArray = [1,2,3,4,5,6,7,8,9,0];
 
-app.get('/numbers', function (req, res) {
+app.get('/numbers', (req, res) => {
 	res.json(myArray);
 })
 
-app.listen(3000, function() {
+app.listen(3000, () => {
 	console.log("Listening on port 3000...");
 })
