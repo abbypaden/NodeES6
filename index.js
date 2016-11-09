b9151eb81d4b499469d8ed2af1758520c634d211
Abigail Paden <Abigail Paden>
2016-11-09 13:38:29 -0500

    added const port to resolve port issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+const port = Number(process.env.PORT || 3000);
 var idx = {index: 0 }
 var str = { str1: "Hello", str2: "World" };
 var num = { num1: 4, num2: 5 };
@@ -16,7 +16,7 @@ app.post('/numbers', (req, res) => {
 	res.send(myArray);
 });
 
-app.listen(3100, () => {
+app.listen(port, () => {
 	console.log("Listening on port 3000...");
 	console.log(idx.str1);
 	console.log(idx.num2);
